eee01855f31ea592da3fa15f55a3f6721b7b0e47
umutSahinkaya <136799358+UmutSahinkayaMakel@users.noreply.github.com>
2023-09-07 13:53:21 +0300

    Searching bitti

---
+++ Repositories/EFCore/Extensions/BookRepositoryExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.EFCore.Extensions
+{
+    public static class BookRepositoryExtensions
+    {
+        public static IQueryable<Book> FilterBooks(this IQueryable<Book> books,
+            uint minPrice, uint maxPrice) =>
+            books.Where(book =>
+            book.Price >= minPrice
+            && book.Price <= maxPrice);
+        public static IQueryable<Book> Search(this IQueryable<Book> books,string searchTerm)
+        {
+            if(string.IsNullOrWhiteSpace(searchTerm))
+                return books;
+            var lowerCaseTerm=searchTerm.Trim().ToLower();
+            return books.Where(book =>
+            book.Title
+            .ToLower()
+            .Contains(lowerCaseTerm));
+        }
+    } 
+}
