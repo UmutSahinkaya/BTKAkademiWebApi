0c6946d4dac4ff7bb85a109e0b0df9b0d8c3d8ab
umutSahinkaya <umutsahinkaya1@gmail.com>
2023-09-18 10:51:52 +0300

    RefreshToken fields added and migration

--- Entities/Models/User.cs
+++ Entities/Models/User.cs
@@ -11,5 +11,7 @@ namespace Entities.Models
     {
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
+        public string? RefreshToken { get; set; }
+        public DateTime RefreshTokenExpireTime { get; set; }
     }
 }
